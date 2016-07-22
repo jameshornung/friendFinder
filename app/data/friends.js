21b965021b80b9de111fbe1f2f93758704477d24
James Hornung <hornung.james@gmail.com>
2016-07-22 16:55:18 -0500

    functional, requires styling, validation, and more questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,15 +2,15 @@ var friends = [
 	{
 		name: 'Donald Trump',
 		photo: 'http://theresurgent.com/wp-content/uploads/2016/02/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1.jpg',
-		scores: [1, 1, 5]
+		scores: [5, 5, 5]
 	},{
 		name: 'Alex Morgan',
 		photo: 'http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg',
-		scores: [1, 5, 1]
+		scores: [4, 4, 4]
 	},{
 		name: 'Phil Dunphy',
 		photo: 'https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg',
-		scores: [5, 5, 3]
+		scores: [3, 3, 3]
 	}];
 
 module.exports = friends;
