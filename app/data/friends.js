c0d83962054087b2174592ed229e9116ddc52149
James Hornung <hornung.james@gmail.com>
2016-07-23 12:31:18 -0500

    removing node_modules for heroku deployment?

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,11 @@ var friends = [
 	{
 		name: 'Donald Trump',
 		photo: 'http://theresurgent.com/wp-content/uploads/2016/02/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1.jpg',
-		scores: [5, 5, 5]
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 	},{
 		name: 'Alex Morgan',
 		photo: 'http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg',
-		scores: [4, 4, 4]
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	},{
 		name: 'Phil Dunphy',
 		photo: 'https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg',
--- server.js
+++ server.js
@@ -31,6 +31,6 @@ require('./app/routing/html-routes.js')(app);
 // Starts the server to begin listening 
 // =============================================================
 
-app.listen(PORT, function(){
-	console.log('App listening on PORT ' + PORT);
+app.listen(process.env.PORT || 3000, function(){
+	console.log('App listening on PORT ' + process.env.PORT);
 });
